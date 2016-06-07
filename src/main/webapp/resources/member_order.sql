dc4c894cd34f90b51dacc4d235a257de832ea2b3
LeeSeoungJea <lsj903602@gmail.com>
2016-06-07 10:01:30 +0900

    아이템, 멤버오더 sql문

---
+++ src/main/webapp/resources/member_order.sql
@@ -0,0 +1,17 @@
+drop table member_order;
+create table member_order(
+	no number primary key,
+	giver number references member (no),
+	taker number references member (no),
+	item varchar2(100) not null,
+	cnt number default 1,
+	orderDate Date default sysdate,
+	state number default 0
+)
+
+drop sequence member_order_seq;
+create sequence member_order_seq
+start with 1
+increment by 1
+nocycle
+nocache;
