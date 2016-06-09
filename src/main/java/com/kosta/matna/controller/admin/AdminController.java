472282d77ef19e1968d506e4e90a738b4ac818bf
LeeSeoungJea <lsj903602@gmail.com>
2016-06-09 15:49:41 +0900

    관리자

---
+++ src/main/java/com/kosta/matna/controller/admin/AdminController.java
@@ -0,0 +1,10 @@
+package com.kosta.matna.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+}
