e0482b85cb8644574033e0ef784469aaaec27641
LeeSeoungJea <lsj903602@gmail.com>
2016-06-09 15:39:09 +0900

    상품2

--- src/main/java/com/kosta/matna/controller/item/ItemController.java
+++ src/main/java/com/kosta/matna/controller/item/ItemController.java
@@ -53,14 +53,14 @@ public class ItemController {
 	public String buy(Model model){// ,int ino, int num, int no, int price){
 		try {
 			
-			//System.out.println("실행!@#");
+			System.out.println("실행!@#");
 			//model.addAttribute("buyItem",service.updateItem(3, -2));//service.update(ino, num));// ino:상품 번호 num:상품 수량 //상품구매시 업데이트
 			//model.addAttribute("buyPoint", service.updatePoint(3, -1000));//service.updatePoint(no, price));// no:회원 번호 price:상품가//상품구매시 가격만큼 포인트 감소
 			service.updateItem(3, -1);
 			service.updatePoint(3, -1000);
 			MemberVO member = service.readMember(3);
 			String addr = member.getAddr();
-			//선물과 구매 /주문내역 생성부분
+			//선물과 구매 /주문내역 생성부분 안함
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
