2c2c57198decfb6906c9cb3c0c958723cd78f621
leecwon <leecwon@naver.com>
2016-06-15 11:46:13 +0900

    수정

--- src/main/java/com/kosta/matna/service/member/MemberService.java
+++ src/main/java/com/kosta/matna/service/member/MemberService.java
@@ -7,7 +7,9 @@ import com.kosta.matna.domain.member.MemberVO;
 public interface MemberService {
 	public void insertMember(MemberVO member)throws Exception;
 	public List<MemberVO> listAll()throws Exception;
+	public List<MemberVO> selectNickname(String nickname)throws Exception;
 	public MemberVO selectID(String id)throws Exception;
+	public MemberVO selectNo(int no)throws Exception;
 	public void update(MemberVO member) throws Exception;
 	public void delete(int no) throws Exception;
 	public boolean checkID(String id)throws Exception;
--- src/main/java/com/kosta/matna/service/member/MemberServiceImpl.java
+++ src/main/java/com/kosta/matna/service/member/MemberServiceImpl.java
@@ -12,47 +12,57 @@ import com.kosta.matna.persistence.member.MemberDAO;
 @Service
 public class MemberServiceImpl implements MemberService {
 
-	@Inject
-	private MemberDAO memberDAO;
+		@Inject
+		private MemberDAO memberDAO;
+		
+		@Override
+		public void insertMember(MemberVO member) throws Exception {
+			memberDAO.insertMember(member);
+		}
+
+		@Override
+		public List<MemberVO> listAll() throws Exception {
+			return memberDAO.selectMember();
+		}
+
+		@Override
+		public MemberVO selectID(String id) throws Exception {
+			return memberDAO.selectID(id);
+		}
+
+		@Override
+		public void update(MemberVO member) throws Exception {
+			memberDAO.update(member);
+		}
+
+		@Override
+		public void delete(int no) throws Exception {
+			memberDAO.delete(no);
+		}
+
+		@Override
+		public boolean checkID(String id) throws Exception {
+			return memberDAO.checkID(id);
+		}
+
+		@Override
+		public boolean checkNickname(String nickname) throws Exception {
+			return memberDAO.checkNickname(nickname);
+		}
+
+		@Override
+		public MemberVO selectIsMember(String id, String pw) throws Exception {
+			return memberDAO.selectIsMember(id, pw);
+		}
+
+		@Override
+		public MemberVO selectNo(int no) throws Exception {
+			return memberDAO.selectNo(no);
+		}
+
+		@Override
+		public List<MemberVO> selectNickname(String nickname) throws Exception {
+			return memberDAO.selectNickname(nickname);
+		}
 	
-	@Override
-	public void insertMember(MemberVO member) throws Exception {
-		memberDAO.insertMember(member);
-	}
-
-	@Override
-	public List<MemberVO> listAll() throws Exception {
-		return memberDAO.selectMember();
-	}
-
-	@Override
-	public MemberVO selectID(String id) throws Exception {
-		return memberDAO.selectID(id);
-	}
-
-	@Override
-	public void update(MemberVO member) throws Exception {
-		memberDAO.update(member);
-	}
-
-	@Override
-	public void delete(int no) throws Exception {
-		memberDAO.delete(no);
-	}
-
-	@Override
-	public boolean checkID(String id) throws Exception {
-		return memberDAO.checkID(id);
-	}
-
-	@Override
-	public boolean checkNickname(String nickname) throws Exception {
-		return memberDAO.checkNickname(nickname);
-	}
-
-	@Override
-	public MemberVO selectIsMember(String id, String pw) throws Exception {
-		return memberDAO.selectIsMember(id, pw);
-	}
-
 }
