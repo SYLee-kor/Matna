34e1adb521b1a8e540e970219bb35481f6a5ff83
SYLEE-kor <hsj1172@gmail.com>
2016-06-29 11:58:31 +0900

    회원 삭제시 폴더삭제,커뮤니티sql바꿈

--- src/main/webapp/resource/sql/community.sql
+++ src/main/webapp/resource/sql/community.sql
@@ -1,14 +1,17 @@
  insert into BOARD_free (no,title,content,writer) (select board_free_seq.nextval, title,content,writer from BOARD_free);
  insert into ad (no,photo,title,comments,link) (select ad_seq.nextval,photo,title,comments,link from ad);
- 
+
+drop sequence board_free_reply_seq;
 drop sequence board_free_seq;
+drop table board_free_reply;
+drop table board_free; 
+
+
 create sequence board_free_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_free;
 create table board_free(
 	no number primary key,
 	title varchar(200) not null,
@@ -18,15 +21,11 @@ create table board_free(
 	replyCnt number default 0,
 	regdate date default sysdate
 );
-
-drop sequence board_free_reply_seq;
 create sequence board_free_reply_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_free_reply;
 create table board_free_reply(
 	no number primary key,
 	bno number references board_free (no) on delete cascade,
@@ -35,14 +34,16 @@ create table board_free_reply(
 	regdate date default sysdate
 );
 
+drop table board_meeting_reply;
+drop table board_meeting;
 drop sequence board_meeting_seq;
+drop sequence board_meeting_reply_seq;
+
 create sequence board_meeting_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_meeting;
 create table board_meeting(
 	no number primary key,
 	title varchar(200) not null,
@@ -52,15 +53,11 @@ create table board_meeting(
 	replyCnt number default 0,
 	regdate date default sysdate
 );
-
-drop sequence board_meeting_reply_seq;
 create sequence board_meeting_reply_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_meeting_reply;
 create table board_meeting_reply(
 	no number primary key,
 	bno number references board_meeting (no) on delete cascade,
@@ -69,14 +66,16 @@ create table board_meeting_reply(
 	regdate date default sysdate
 );
 
+drop table board_noname_reply;
+drop table board_noname;
 drop sequence board_noname_seq;
+drop sequence board_noname_reply_seq;
+
 create sequence board_noname_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_noname;
 create table board_noname(
 	no number primary key,
 	title varchar(200) not null,
@@ -86,15 +85,11 @@ create table board_noname(
 	replyCnt number default 0,
 	regdate date default sysdate
 );
-
-drop sequence board_noname_reply_seq;
 create sequence board_noname_reply_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_noname_reply;
 create table board_noname_reply(
 	no number primary key,
 	bno number references board_noname (no) on delete cascade,
@@ -103,14 +98,16 @@ create table board_noname_reply(
 	regdate date default sysdate
 );
 
+drop table board_notice_reply;
+drop table board_notice;
 drop sequence board_notice_seq;
+drop sequence board_notice_reply_seq;
+
 create sequence board_notice_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_notice;
 create table board_notice(
 	no number primary key,
 	title varchar(200) not null,
@@ -120,15 +117,11 @@ create table board_notice(
 	replyCnt number default 0,
 	regdate date default sysdate
 );
-
-drop sequence board_notice_reply_seq;
 create sequence board_notice_reply_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_notice_reply;
 create table board_notice_reply(
 	no number primary key,
 	bno number references board_notice (no) on delete cascade,
@@ -137,14 +130,16 @@ create table board_notice_reply(
 	regdate date default sysdate
 );
 
+drop table board_suggest_reply;
+drop table board_suggest;
 drop sequence board_suggest_seq;
+drop sequence board_suggest_reply_seq;
+
 create sequence board_suggest_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_suggest;
 create table board_suggest(
 	no number primary key,
 	title varchar(200) not null,
@@ -154,15 +149,11 @@ create table board_suggest(
 	replyCnt number default 0,
 	regdate date default sysdate
 );
-
-drop sequence board_suggest_reply_seq;
 create sequence board_suggest_reply_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_suggest_reply;
 create table board_suggest_reply(
 	no number primary key,
 	bno number references board_suggest (no) on delete cascade,
@@ -170,15 +161,16 @@ create table board_suggest_reply(
 	content varchar(300),
 	regdate date default sysdate
 );
-
+drop table board_faq_reply;
+drop table board_faq;
 drop sequence board_faq_seq;
+drop sequence board_faq_reply_seq;
+
 create sequence board_faq_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_faq;
 create table board_faq(
 	no number primary key,
 	title varchar(200) not null,
@@ -188,15 +180,11 @@ create table board_faq(
 	replyCnt number default 0,
 	regdate date default sysdate
 );
-
-drop sequence board_faq_reply_seq;
 create sequence board_faq_reply_seq
 start with 1
 increment by 1
 nocycle
 nocache;
-
-drop table board_faq_reply;
 create table board_faq_reply(
 	no number primary key,
 	bno number references board_faq (no) on delete cascade,
